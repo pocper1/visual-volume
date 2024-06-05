c47e6f7723d8d959732c53f2d6d8cf21d31ccf13
pocper1 <pocper1@gmail.com>
2024-06-06 04:36:59 +0800

    0606-finish project-2
    Former-commit-id: eb83248b50effb9c91ae6471734e561d1f7a4064

--- code/read_nrrd.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import nrrd
-
-if __name__ == "__main__":
-    filename = "../web/public/sobel_vectors.nrrd"
-
-    readdata, header = nrrd.read(filename)
-    print(readdata.shape)
-    print(header)
